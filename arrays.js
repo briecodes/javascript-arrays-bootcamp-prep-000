b6c12d65265216a093b758ab503ab75ed57b47a6
Brian A Morales <bombibills@gmail.com>
2018-02-28 18:11:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array;
+  newArray.unshift(element);
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+}
